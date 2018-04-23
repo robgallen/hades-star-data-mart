b1e072cc7e2dd874e2ac028a3b315b4cc2ca3398
Robert Allen <robgallen@gmail.com>
2018-04-23 20:27:54 +0100

    Fix some unit tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,9 @@ module.exports = {
 		browser
 			.url(devServer)
 			.waitForElementVisible("#app", 5000)
-			.assert.elementPresent(".hello")
-			.assert.containsText("h1", "Welcome to Your Vue.js App")
-			.assert.elementCount("img", 1)
+			.assert.elementPresent(".container")
+			.assert.containsText("h1", "Welcome to the Hades' Star Data Mart")
+			.assert.elementCount("ul", 2)
 			.end();
 	}
 };
